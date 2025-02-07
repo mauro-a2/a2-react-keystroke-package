d291c6091fef957897621646a56b76a2aad9a79b
Mauro Ruiz <mauro@area2.ai>
2025-02-07 17:18:30 +0100

    Fix: keystroke data typos

--- src/api/actions/get-reduced-neuroprofile.ts
+++ src/api/actions/get-reduced-neuroprofile.ts
@@ -1,8 +1,9 @@
 import type { AxiosError } from "axios";
 import type { IKeystrokeCollection, IMobileKeystrokeCollection } from "@area2-ai/a2-node-keystroke-package";
 
-import type { IA2APIResponse, IA2ChatbotResults } from "../../interfaces";
 import a2API from "../area2-api";
+import { formatKeystrokeData } from "../../helpers";
+import type { IA2APIResponse, IA2ChatbotResults } from "../../interfaces";
 
 export type NeuroprofileResponse = {
     ok: boolean;
@@ -17,6 +18,7 @@ export type NeuroprofileResponse = {
  * @param {string} userID - The unique identifier of the user for whom the neuroprofile is generated.
  * @param {string} token - A token used for authentication or authorization purposes.
  * @param {IKeystrokeCollection | IMobileKeystrokeCollection} typingData - The user's typing data, which can be either desktop or mobile keystroke collections.
+ * @param {'Desktop' | 'Mobile'} platform - The platform type indicating whether the typing data is from a desktop or mobile device.
  * @returns {Promise<NeuroprofileResponse>} A promise that resolves to a NeuroprofileResponse containing the reduced neuroprofile data.
  */
 
@@ -24,21 +26,16 @@ export const getReducedNeuroprofile = async (
     userID: string,
     token: string,
     typingData: IKeystrokeCollection | IMobileKeystrokeCollection,
+    platform: 'Desktop' | 'Mobile',
 ): Promise<NeuroprofileResponse> => {
 
-    const {
-        startUnixTime,
-        timeZone,
-        ...rest
-    } = typingData;
+    const formattedBody = formatKeystrokeData(platform, typingData);
 
     const dataToSend = {
         'a2_actions': ["a2_chatbot"],
         'keystroke_data': {
-            ...rest,
+            ...formattedBody,
             'user_id': userID,
-            'startunixtime': startUnixTime,
-            'timezone': timeZone,
         }
     }
 
