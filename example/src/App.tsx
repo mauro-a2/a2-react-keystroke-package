6c73427536aa43083cbae8f2731efc2520a5f7b6
Mauro Ruiz <mauro@area2.ai>
2025-10-15 18:36:31 +0200

    Update: Example Updated

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,27 @@
-import { DesktopInput } from './components/DesktopInput';
-
-//! Also check AndroidInput and IosInput implementations
+import { useState } from 'react';
+import { A2Textbox, useCipherCapture, } from '@area2-ai/a2-react-keystroke-package';
 
 export const App = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const { handleEndTypingSession } = useCipherCapture();
+
+  //!Nota: Recordar que al establecer el target en el hook, también se debe establecer en el componente
+
+  // TODO iOS: Necesario para detectar cambios de auto-corrección
+  // useEffect(() => {
+  //   processAutoCorrection(inputValue);
+  // }, [inputValue]);
+
   return (
-    <>
-      <DesktopInput />
-    </>
+    <div>
+      <A2Textbox
+        style={{ width: '300px' }}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleEndTypingSession}>Send</button>
+    </div>
   )
 }
 
